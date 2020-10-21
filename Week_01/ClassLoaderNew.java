cd06b3e177b3a8a5ef2febe8220efa025ed2b074
xjf <954475782@qq.com>
2020-10-21 22:40:27 +0800

    add

--- Week_01/ClassLoaderNew.java
+++ Week_01/ClassLoaderNew.java
@@ -1,30 +1,62 @@
 package Week_01;
 
+/**
+ * 作业二，加载自定义文件，通过反射调用方法
+ */
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class ClassLoaderNew extends ClassLoader {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws Exception {
+        Object hello = new ClassLoaderNew().findClass("Hello").newInstance();
+        hello.getClass().getDeclaredMethod("hello").invoke(hello);
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        File f = new File(this.getClass().getResource("/Hello.xlass").getPath());
-        int length = (int)f.length();
-        byte[] bytes = new byte[length];
+
+        byte[] bytes;
         try {
-            new FileInputStream(f).read(bytes);
+            bytes = toByteArray("Week_01/Hello.xlass");
         } catch (IOException e) {
             e.printStackTrace();
             return super.findClass(name);
         }
         for (int i = 0; i < bytes.length; ++i) {
-            bytes[i] = (byte)(255 - bytes[i]);
+            bytes[i] = (byte) (255 - bytes[i]);
         }
         return defineClass(name, bytes, 0, bytes.length);
+    }
+
+    public static byte[] toByteArray(String filename) throws IOException {
+
+        File f = new File(filename);
+        if (!f.exists()) {
+            throw new FileNotFoundException(filename);
+        }
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream((int) f.length());
+        BufferedInputStream in = null;
+        try {
+            in = new BufferedInputStream(new FileInputStream(f));
+            int buf_size = 1024;
+            byte[] buffer = new byte[buf_size];
+            int len = 0;
+            while (-1 != (len = in.read(buffer, 0, buf_size))) {
+                bos.write(buffer, 0, len);
+            }
+            return bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            bos.close();
+        }
     }
 
 }
